b76d361d35ce2fc8fa756181323c3249d0ca6f4c
Nikita <nikita.kopylov2003@gmail.com>
2022-09-11 12:22:25 +0300

    Nigga

--- KOFFL.cpp
+++ KOFFL.cpp
@@ -6,13 +6,44 @@ using namespace std;
 
 class Human
 {
-	int health = 2;
-	int damage = 2;
-	float block = 0.5;
-	int money = 50;
-	//добавьте конструкторы, чтобы легче было потом создавать рандомных челиков с разным кол-вом жизни, урона и тд  
+public:
+	string name;
+	void Upgrade_Health();
+	void Upgrade_Strenght();
 };
+class Person: public Human {
+	int species; 
+	int health;
+	int money; 
+	char name;
+	int damage;
+	float block;
+	Person(int species){
+		switch (species)
+		{
+		case 1:
+			health = 100;
+			money = 1000;
+			damage = 10;
+			block = 0.5;
+			//Bogatyr
 
+		case 2:
+			health = 5;
+			money = 10000;
+			damage = 6;
+			block = 0;
+			//thief
+
+		case 3: 
+			health = 4;
+			money = 108;
+			damage = 20;
+			block = 0.7;
+		}
+	}
+
+};
 void Menu() {
 	string Zero = "Choose your variant\n\n\n";
 	string First = "[1] Armour\n";
@@ -27,14 +58,20 @@ void Menu() {
 class Armour {
 public:
 	long int cost;
+
 	list <string> clothes = { "[1] Boots", "[2] Нагрудник", "[3] Шлем" };
 	list <string> weapeon = { "[1] Алмазный меч", "[2] Лук", "[3] Арбалет" };
+	//прочитайте про контейнер map -> и замените два list на map ключом будет число,значением строка 11.09.2022
+
 	void Print_Armour();
 	void Print_Clothes();
 
+	//создай методы которые позволяют добавить экипировку персонажу(human) типо я выбрал шлем это означает плюс к броне.
+
 };
+
 void Print_Armour() {
-	cout << "[1] Weapon\n" << "[2] Clothes" << endl;
+	cout << "[1] Weapon\n" << "[2] Clothes" << endl; //тут в зависимости от выбора ты выводишь содержимое каждой котегории 11.09.2022.
 }
 void Print_Clothes() {
 	cout << endl;
@@ -51,11 +88,17 @@ class Discribtion
 
 int main() {
 	cout << "Write your name please" << endl;
-	string name;
+	Human name;
+	cin >> name.name;//после того как добавите имя персонажа в класс human
+	//создайте тут класс human Human gus(fred,20);20-> это возраст 
+	//но можно по другому создать класс Person который наследуется от human. Это позволит в будем создавть разных людей.
+	//т.к класс human -> это человечество -> Person -> это будет один человек. А в человестве люди разные. Поэтому в person можно создавать разных типов людей.
 	cin >> name;
 	Menu();
 	int variant;
 	cin >> variant;
+	//можно добавить action внутри персонажа типо у вас есть Объект человек -> внутри есть "что-то" что позволяет давать ему команды и чтоб она выполнял их.
+	//например Person hero("Fred",20); hero.figth(), hero.run(), hero.earn(), hero.jump(), hero.regenerate(), hero.die(), hero.changePower() и тд
 	switch (variant)
 	{
 	case 1:
